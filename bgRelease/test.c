4ed35484073c6b414f601389b42f8bf32f58e77a
Bright Gao <brightgao1234@gmail.com>
2025-12-17 20:52:08 -0600

    BrightEditor autopush 20:52:07 12/17/2025 CST

---
+++ bgRelease/test.c
@@ -0,0 +1,10 @@
+༼        Created w/ BrightWin & BrightEditor (both written by Bright Gao)
+
+Creation time & date:     20:52:02 , 12/17/2,025
+
+File author:              Bright Gao
+
+File name when created:   test.c
+
+Directory created in:     C:\Users\brigh\Documents\zkyProjects\pcCompiler\bgRelease        ༽
+
